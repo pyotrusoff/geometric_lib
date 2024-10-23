767c7ae0d5c794a0911404b6d1ba4cc66a463cac
Petr Usov <78650389+pyotrusoff@users.noreply.github.com>
2024-10-23 11:12:51 +0300

    Update triangle.py

--- triangle.py
+++ triangle.py
@@ -1,5 +1,13 @@
+'''Parameters: 
+a(int) triangle side
+b(int) another trianle side
+c(int) 3rd triangle side
+h(int) triangle height
+'''
 def area(a, h): 
     return a * h / 2 
+    "returns area of given triangle"
 
 def perimeter(a, b, c): 
     return a + b + c 
+    "returns perimeter of given triangle"
