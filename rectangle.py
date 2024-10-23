fc50c405b2054695b43d8874fb887f8689f26239
Petr Usov <78650389+pyotrusoff@users.noreply.github.com>
2024-10-23 11:06:14 +0300

    Update rectangle.py

--- rectangle.py
+++ rectangle.py
@@ -1,6 +1,8 @@
-
+"Parameters: a(int) rectangle side"
+"Parameters: b(int) 2nd rectangle side"
 def area(a, b): 
     return a * b 
-
+    "returns area of given rectangle"
 def perimeter(a, b): 
     return (a + b) * 2 
+    "returns perimeter of given rectangle"
