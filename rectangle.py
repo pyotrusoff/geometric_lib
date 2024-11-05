703cebea27d66ca4ab6251f4429c849b83b193f1
Petr Usov <78650389+pyotrusoff@users.noreply.github.com>
2024-11-05 12:26:46 +0300

    Update rectangle.py

--- rectangle.py
+++ rectangle.py
@@ -1,10 +1,30 @@
-'''Parameters: 
-a(int) rectangle side
-b(int) another rectangle side
-'''
 def area(a, b): 
+'''
+This function returns the area of given rectangle
+
+Parameters: 
+    a(int) rectangle side
+    b(int) another rectangle side
+
+Return value:
+    (int): area of the rectangle with sides a and b
+'''
+
     return a * b 
-    "returns area of given rectangle"
+
+
+
 def perimeter(a, b): 
+'''
+This function returns the perimeter of given rectangle
+
+Parameters: 
+    a(int) rectangle side
+    b(int) another rectangle side
+
+Return value:
+    (int): perimeter of the rectangle with sides a and b
+'''
+
     return (a + b) * 2 
-    "returns perimeter of given rectangle"
+
