280b2092e1267efa7d421bfd7d3652d6532edb9a
Petr Usov <78650389+pyotrusoff@users.noreply.github.com>
2024-11-05 12:28:23 +0300

    Update square.py

--- square.py
+++ square.py
@@ -1,8 +1,27 @@
-"Parameters: a(int) sqare side"
 def area(a):
+'''
+This function returns the area of given sqare
+
+Parameters:
+    a(int) sqare side
+
+Return value:
+    (int): area of the sqare with side a
+
+'''
+    
     return a * a
-    "returns area of given sqare"
 
 def perimeter(a):
+'''
+This function returns the perimeter of given sqare
+
+Parameters:
+    a(int) sqare side
+
+Return value:
+    (int): perimeter of the sqare with side a
+
+'''
+    
     return 4 * a
-    "returns perimeter of given sqare"
